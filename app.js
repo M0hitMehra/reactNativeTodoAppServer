35a50e35d2c25b97a920027eef09d036a9cc66df
Mohit Mehra <98013205+M0hitMehra@users.noreply.github.com>
2023-06-11 22:33:33 +0530

    second  commit

--- app.js
+++ app.js
@@ -2,17 +2,23 @@ import express from "express";
 export const app = express();
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
-import cors from "cors"
+import cors from "cors";
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
-app.use(fileUpload({
-    limits:{fileSize:50*1024*1024},
-    useTempFiles:true
-}))
-app.use(cors())
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+    useTempFiles: true,
+  })
+);
+app.use(cors());
 
 // routes
 import userRoutes from "./routers/userRoutes.js";
 app.use("/api/v1", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Server is working");
+});
